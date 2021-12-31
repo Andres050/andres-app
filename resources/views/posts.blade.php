7ed010664d3a444efdcaecc13a24e5bb05e57977
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-31 17:08:36 +0100

    Video 11

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,23 +5,10 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-<article>
-    <h1><a href="/posts/my-first-post">My First Post</a></h1>
-    <p>
-        Un texto es una composición de signos codificados en un sistema de escritura que forma una unidad de sentido. También es una composición de caracteres imprimibles generados por un algoritmo de cifrado que, aunque no tienen sentido para cualquier persona, sí puede ser descifrado por su destinatario original
-    </p>
-</article>
-<article>
-    <h1><a  href="/posts/my-second-post">My Second Post</a></h1>
-    <p>
-        Un texto es una composición de signos codificados en un sistema de escritura que forma una unidad de sentido. También es una composición de caracteres imprimibles generados por un algoritmo de cifrado que, aunque no tienen sentido para cualquier persona, sí puede ser descifrado por su destinatario original
-    </p>
-</article>
-<article>
-    <h1><a  href="/posts/my-third-post">My Third Post</a></h1>
-    <p>
-        Un texto es una composición de signos codificados en un sistema de escritura que forma una unidad de sentido. También es una composición de caracteres imprimibles generados por un algoritmo de cifrado que, aunque no tienen sentido para cualquier persona, sí puede ser descifrado por su destinatario original
-    </p>
-</article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?=$post; ?>
+        </article>
+    <?php endforeach;?>
 </body>
 </html>
