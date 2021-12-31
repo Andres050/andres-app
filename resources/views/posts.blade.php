bcfb1c8ce5f34482bbfb995595a5f89d28b3ce3d
Andres Garcia Bauza <agarcia15183@alumnes.iesmanacor.cat>
2021-12-31 16:04:42 +0100

    Video 7 started

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ies Manacor</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+<script src="/app.js"></script>
+<h1>Hello World</h1>
+</body>
+</html>
