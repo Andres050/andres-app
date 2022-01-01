ac09ea62a3cc9c45485bf9aab08d61dec280735a
Andres Garcia Bauza <militaxx5@gmail.com>
2022-01-01 22:10:29 +0100

    Video 27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Call of Duty (en español: «La llamada del deber») es una serie de unos videojuegos
+            de disparos en primera persona, de estilo bélico, desarrollada principal e inicialmente por Infinity Ward,
+            Treyarch, Sledgehammer Games y en menor proporción Raven Software y distribuida por Activision.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Call of Duty (en español: «La llamada del deber») es una serie de unos videojuegos
+            de disparos en primera persona, de estilo bélico, desarrollada principal e inicialmente por Infinity Ward,
+            Treyarch, Sledgehammer Games y en menor proporción Raven Software y distribuida por Activision.</p>'
+        ]);
     }
 }
