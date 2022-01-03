cc7359301d4803adc567aae8096ea6140e03dd67
Andres Garcia Bauza <militaxx5@gmail.com>
2022-01-04 00:22:03 +0100

    Video 41

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -40,5 +40,11 @@ class Post extends Model
                $query->where('slug', $category);
             });
         });
+
+        $query->when($filters['author'] ?? false, function ($query, $author) {
+            $query->wherehas('author', function ($query) use ($author) {
+                $query->where('username', $author);
+            });
+        });
     }
 }
