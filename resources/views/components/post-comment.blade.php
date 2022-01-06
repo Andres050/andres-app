c9f089a288764b3f90b2b719f5c703bade28dc01
Andres Garcia Bauza <militaxx5@gmail.com>
2022-01-06 15:09:13 +0100

    Video 55

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,23 +1,25 @@
 @props(['comment'])
 
+<x-panel class="bg-gray-50">
+    <article class="flex space-x">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/?u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
+        </div>
 
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/?u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
-    </div>
+        <div style="padding-left: 20px">
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
 
-    <div style="padding-left: 20px">
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{$comment->created_at->diffForHumans()}}</time>
+                </p>
+            </header>
 
-            <p class="text-xs">
-                Posted
-                <time>{{$comment->created_at->diffForHumans()}}</time>
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
+        </div>
+    </article>
+</x-panel>
 
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
