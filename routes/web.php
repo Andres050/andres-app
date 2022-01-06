7f663df8ef20b497d2dd7d9446355a5fdb2bf89c
Andres Garcia Bauza <militaxx5@gmail.com>
2022-01-06 20:11:58 +0100

    Video 58 solucionado tambien tema ssl

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us20'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('ad41a7f285', [
+        'email_address' => 'militaxx521313@gmail.com',
+        'status' => 'subscribed'
+    ]);
+
+    ddd($response);
+});
 
 Route::get('/',[PostController::class, 'index'])->name('home');
 
